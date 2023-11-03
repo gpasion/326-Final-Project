78fb91be2eaa9e9acc2bfe924d2929968387c64a
joellebanim1 <136139818+joellembanim@users.noreply.github.com>
2023-11-03 15:00:12 -0400

    RecipeDescription

---
+++ RecipeDecription.py
@@ -0,0 +1,15 @@
+import re 
+import os
+
+with open("step2output_template.txt" , "r") as file:
+    text = file.read()
+
+#this line is importung the text file that lists ingredients"""
+
+#will use regex in the following lines to give recipe details"""
+
+pattern = r' (Recipe Title: ) (.*)'
+
+title = "Recipe for your ingredients"
+
+updated_text = re.sub( pattern, r'\1\n' + title)
