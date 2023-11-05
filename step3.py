a8163c0c6bb987f78607a1450c2ce14a6a56964c
israel300830 <israel30@terpmail.umd.edu>
2023-11-05 18:53:17 -0500

    Recipe quantity updated
    just added the ounce values to the recipes

--- step3.py
+++ step3.py
@@ -36,37 +36,3 @@ for file_path in file_paths:
 # Print the extracted ingredients for each recipe
 for recipe, ingredients in recipes.items():
     print(f'{recipe}: {ingredients}')
-
-'''# Initialize items_data dictionary
-items_data = {}
-
-# Read CSV data from a file named 'data.csv' in the current directory
-with open('Food_Database.csv', 'r', encoding='utf-8') as csvfile:
-    csv_reader = csv.DictReader(csvfile)
-    for row in csv_reader:
-        item_name = row.get('item_name')
-        price = row.get('price')
-        amount = row.get('amount(lbs/oz/ct(count))')
-        link = row.get('link(url)')
-
-        # Check if all required fields are present and non-empty
-        if item_name and price and amount and link:
-            items_data[item_name] = {
-                "price": float(price.strip('$')),
-                "amount": amount,
-                "link": link
-            }
-        else:
-            print(f"Skipping malformed data: {row}")
-
-# Generate output
-for item, quantity in input_data.items():
-    if item in items_data:
-        price_per_unit = items_data[item]["price"]
-        total_price = price_per_unit * float(quantity.split()[0])  # Extract numeric quantity for calculations
-        formatted_price = "${:.2f}".format(total_price)
-        output = f"Item: {item}, amount: {quantity}, price: {formatted_price}, link: {items_data[item]['link']}"
-        print(output)
-    else:
-        print(f"Item '{item}' not found in the CSV data.")
-        '''
