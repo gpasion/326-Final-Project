f1f478abcdfd204a1910e7acaccca9fa69fa5d45
israel300830 <israel30@terpmail.umd.edu>
2023-12-15 02:18:28 -0500

    Update parse_recipe_test.py

--- tests/parse_recipe_test.py
+++ tests/parse_recipe_test.py
@@ -2,32 +2,32 @@ import unittest
 from recipe_utils import parse_recipe_from_file
 
 class TestParseRecipeFromFile(unittest.TestCase):
-    def test_quick_and_savory_recipe(self):
+    def easy_recipe(self):
         recipe = parse_recipe_from_file('recipes/easy_recipe.txt', 'Food_Database.csv')
 
         self.assertRegex(recipe.title, r'.+')  # Checking for any non-empty title
         self.assertRegex(recipe.cook_time, r'\d+\s*(?:Minutes|minutes)')
-        self.assertIn(recipe.difficulty, ['Easy', 'Intermediate', 'Advanced'])
+        self.assertIn(recipe.difficulty, ['Easy'])
         self.assertIsInstance(recipe.servings, str)
         self.assertIsInstance(recipe.utensils, list)
         self.assertIsInstance(recipe.ingredients, list)
 
-    def test_roasted_recipe(self):
+    def intermediate_recipe(self):
         recipe = parse_recipe_from_file('recipes/intermediate_recipe.txt', 'Food_Database.csv')
 
         self.assertRegex(recipe.title, r'.+')  # Checking for any non-empty title
         self.assertRegex(recipe.cook_time, r'\d+\s*(?:Minutes|minutes)')
-        self.assertIn(recipe.difficulty, ['Easy', 'Intermediate', 'Advanced'])
+        self.assertIn(recipe.difficulty, ['Intermediate'])
         self.assertIsInstance(recipe.servings, str)
         self.assertIsInstance(recipe.utensils, list)
         self.assertIsInstance(recipe.ingredients, list)
 
-    def test_slow_cooked_recipe(self):
+    def advanced_recipe(self):
         recipe = parse_recipe_from_file('recipes/advanced_recipe.txt', 'Food_Database.csv')
 
         self.assertRegex(recipe.title, r'.+')  # Checking for any non-empty title
         self.assertRegex(recipe.cook_time, r'\d+\s*(?:Minutes|minutes)')
-        self.assertIn(recipe.difficulty, ['Easy', 'Intermediate', 'Advanced'])
+        self.assertIn(recipe.difficulty, ['Advanced'])
         self.assertIsInstance(recipe.servings, str)
         self.assertIsInstance(recipe.utensils, list)
         self.assertIsInstance(recipe.ingredients, list)
